c410fec17d787684764f1d522951accbd7108973
Anas-archh <mn10031@st.habib.edu.pk>
2025-10-15 23:49:54 +0500

    Muzammil king

--- src/linkedlist.cpp
+++ src/linkedlist.cpp
@@ -34,32 +34,116 @@ public:
     // Function to print all elements in the linked list
     void print() {
         // TODO: Traverse and print all elements
+        if (!head) {
+            cout << "List is empty." << endl;
+            return;
+        }
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "null" << endl;
     }
+
 
     // Function to calculate the length of the linked list
     int length() {
         // TODO: Traverse and count all nodes
-        return 0;
+        int count = 0;
+        Node* temp = head;
+        while (temp != nullptr) {
+            count++;
+            temp = temp->next;
+        }
+        return count;
     }
+
+
 
     // Function to insert before a specific value
     void insertBefore(int target, int newValue) {
         // TODO: Insert newValue before target node
+        Node* newNode = new Node(newValue);
+        if (!head) {
+            delete newNode;
+            return;
+        }
+
+        if (head->data == target) { 
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+
+        Node* current = head;
+        while (current->next != nullptr && current->next->data != target) {
+            current = current->next;
+        }
+
+        if (current->next != nullptr) {
+            newNode->next = current->next;
+            current->next = newNode;
+        } else {
+             delete newNode;
+        }
     }
 
     // Function to insert after a specific value
     void insertAfter(int target, int newValue) {
         // TODO: Insert newValue after target node
+        Node* current = head;
+        while (current != nullptr && current->data != target) {
+            current = current->next;
+        }
+
+        if (current != nullptr) { // Target found
+            Node* newNode = new Node(newValue);
+            newNode->next = current->next;
+            current->next = newNode;
+        }
     }
 
     // Function to delete node before a specific value
     void deleteBefore(int target) {
         // TODO: Delete node before target node
+        if (!head || !head->next) return;
+
+        if (head->next->data == target) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return;
+        }
+
+        Node* current = head;
+        while (current->next != nullptr && current->next->next != nullptr) {
+            if (current->next->next->data == target) {
+                Node* temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+                return;
+            }
+            current = current->next;
+        }
     }
+
 
     // Function to delete node after a specific value
     void deleteAfter(int target) {
         // TODO: Delete node after target node
+
+        Node* current = head;
+        while (current != nullptr && current->data != target) {
+            current = current->next;
+        }
+
+        // If target is found and it's not the last node
+        if (current != nullptr && current->next != nullptr) {
+            Node* temp = current->next;
+            current->next = temp->next; // Or current->next->next
+            delete temp;
+        }
     }
 };
 
